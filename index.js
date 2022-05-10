a134b15291322902a52149713bd7c8f7d1c5ffbd
ShashankBargali <shashankbargali@gmail.com>
2022-05-10 11:33:06 +0530

    Added port variable

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express();
 
 const port = process.env.PORT || 5000;
-// const {connectPayHome} = require('./db')
-// const cors = require('cors')
+const {connectPayHome} = require('./db')
+const cors = require('cors')
 
-// connectPayHome();
-// app.use(express.json());
+connectPayHome();
+app.use(express.json());
 
 
-// app.use(cors())
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/user', require('./routes/user'))
+app.use(cors())
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/user', require('./routes/user'))
 app.get('/', (req, res)=> {
     res.send("hi wat'up")
 })
