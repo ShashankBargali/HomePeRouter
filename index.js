369713d8114f3df22b0aa6b4c214345279176138
ShashankBargali <shashankbargali@gmail.com>
2022-05-10 07:46:25 +0530

    Testing

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express();
-const {connectPayHome} = require('./db')
-const cors = require('cors')
+// const {connectPayHome} = require('./db')
+// const cors = require('cors')
 
-connectPayHome();
-app.use(express.json());
+// connectPayHome();
+// app.use(express.json());
 
 
-app.use(cors())
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/user', require('./routes/user'))
+// app.use(cors())
+// app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/user', require('./routes/user'))
+app.get('/', (req, res)=> {
+    res.send("hi wat'up")
+})
 
 app.listen(8000, ()=>{
     console.log('Server started at port 8000')
